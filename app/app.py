da58d943553cfbb8d2b380ef317873859f261822
Andrei Sharafullin <andrei.sharafullin@tomtom.com>
2020-07-24 13:36:33 +0200

    changed port

--- app/app.py
+++ app/app.py
@@ -8,4 +8,4 @@ def hello():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8123)
+    app.run(debug=False, host='0.0.0.0', port=80)
